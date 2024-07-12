2d026b1c7dc0909a8af8e9594e95fc1ea8bb1ffd
Ayush Kumar <97400680+ayushchahat@users.noreply.github.com>
2024-07-12 23:30:18 +0000

    Friend Classes & Member Friend Functions in C++

--- tut8.cpp
+++ tut8.cpp
@@ -2,8 +2,11 @@ class Complex
 {
     int a, b;
     // Individually declaring functions as friends
-     friend int Calculator ::sumRealComplex(Complex, Complex);
-     friend int Calculator ::sumCompComplex(Complex, Complex);
+    // friend int Calculator ::sumRealComplex(Complex, Complex);
+    // friend int Calculator ::sumCompComplex(Complex, Complex);
+
+    // Aliter: Declaring the entire calculator class as friend
+    friend class Calculator;
 
 public:
     void setNumber(int n1, int n2)
@@ -18,7 +21,6 @@ public:
     }
 };
 
-
 int Calculator ::sumRealComplex(Complex o1, Complex o2)
 {
     return (o1.a + o2.a);
@@ -28,6 +30,7 @@ int Calculator ::sumCompComplex(Complex o1, Complex o2)
 {
     return (o1.b + o2.b);
 }
+
 int main()
 {
     Complex o1, o2;
